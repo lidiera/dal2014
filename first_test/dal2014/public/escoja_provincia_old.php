100ae7eebcb335cc2dead0f839475a2ffaa293dc
Jose Artavia <lidiera@codeeze.net>
2014-10-26 04:01:05 -0600

    Checkpoint

---
+++ first_test/dal2014/public/escoja_provincia_old.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <!-- META SECTION -->
+        <title>Escoja Provincia</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="css/wdal2014.css"/>             
+        <!-- EOF CSS INCLUDE -->
+    </head>
+    <body>
+       <div>
+            <h1> Escoja provincia</h1>
+
+            <div>
+                <ul>
+                    <li> <a href=""> Alajuela </a> </li>
+                    <li> <a href=""> Cartago </a> </li>
+                    <li> <a href=""> Guanacaste </a> </li>
+                    <li> <a href=""> Heredia </a> </li>
+                    <li> <a href=""> Limón </a> </li>
+                    <li> <a href=""> Puntarenas </a> </li>
+                    <li> <a href="escoja_canton.php"> San José </a> </li>
+                </ul>
+                
+
+            </div>
+       </div> 
+    </body>
+</html>
+
+
+
+
+
+
